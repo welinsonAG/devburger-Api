7e128afd99d11beecffb782c86fdaeea39708e2c
welinsonAG <welindevgit.0@gmail.com>
2026-03-02 20:03:38 -0300

    commit de produção

--- src/server.js
+++ src/server.js
@@ -1,6 +1,25 @@
+// localmente
+//import app from './app.js';
 
-import app from './app.js';
+//const PORT =3001;
 
-const PORT =3001;
+//app.listen(PORT, '0.0.0.0',() => console.log(`Server is running at port ${PORT}...` ));//
 
-app.listen(PORT, '0.0.0.0',() => console.log(`Server is running at port ${PORT}...` ));
+
+//vercel dev
+
+import express from 'express';
+import dotenv, { config } from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+import routes from './routes.js';
+app.use(routes);
+
+export default app;
