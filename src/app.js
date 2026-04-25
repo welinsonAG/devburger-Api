0c8baada12aa3efbb0c88dcd1311fd9ff0da86e7
welinsonAG <welindevgit.0@gmail.com>
2026-04-25 16:43:35 -0300

    commity

--- src/app.js
+++ src/app.js
@@ -18,8 +18,9 @@ constructor() {
 this.app.use(
   cors({
  origin:[
-    'http://localhost:5173',
+  
     'https://devburger-interface-chi.vercel.app',
+      'http://localhost:5173',
  ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
